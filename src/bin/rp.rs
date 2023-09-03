89131cc263461b8e791de14f4d40993fb9595223
Hans Tognon <learningway403@gmail.com>
2023-09-03 23:24:57 +0100

    init - draft a cli

--- src/bin/rp.rs
+++ src/bin/rp.rs
@@ -1,3 +1,35 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about)]
+#[command(propagate_version = true)]
+struct Rp {
+    #[arg(short, long)]
+    explain: bool,
+
+    #[arg(short, long)]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Genkey {
+        private_keys_dir: PathBuf,
+    },
+    Pubkey {
+        private_keys_dir: PathBuf,
+        public_keys_dir: PathBuf,
+    },
+    // TODO: Add options and arguments for Exchange
+    Exchange {},
+}
+
 fn main() {
-    unimplemented!();
+    let rp = Rp::parse();
+    println!("{:?}", rp);
 }
