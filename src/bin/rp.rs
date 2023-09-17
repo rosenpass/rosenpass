8f24407de8cf418a154ec3f53aefbbb18ded63c2
Hans Tognon <learningway403@gmail.com>
2023-09-17 17:41:13 +0100

    chore: savepoint for prototyping

--- src/bin/rp.rs
+++ src/bin/rp.rs
@@ -1,5 +1,6 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, process::Command, fs::{self, File}};
 
+use anyhow::bail;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
@@ -30,12 +31,25 @@ enum Commands {
 }
 
 impl Rp {
-    fn run(self) {
+    fn run(self) -> anyhow::Result<()> {
         use Commands::*;
 
         match self.command {
             Genkey { private_keys_dir } => {
-                println!("Generating key pair in {:?}", private_keys_dir);
+                match private_keys_dir.try_exists() {
+                    Ok(false) => {
+                        println!("Generating key pair in {:?}", private_keys_dir);
+                        let _ = fs::create_dir_all(&private_keys_dir).or_else(|e| {
+                            bail!("Error creating directory {:?}: {}", private_keys_dir, e)
+                        });
+                        let wg_key = File::create(private_keys_dir.join("wgsk"))?;
+                        let wireguard_keygen = Command::new("wg").args(["genkey"]).stdout(wg_key).output()?;
+                        println!("{wireguard_keygen:?}");
+                    }
+                    Ok(true) => bail!("PRIVATE_KEYS_DIR {:?} already exists", private_keys_dir),
+                    Err(e) => bail!("Error checking for directory {:?}: {}", private_keys_dir, e),
+                }
+                Ok(())
             }
             Pubkey {
                 private_keys_dir,
@@ -45,15 +59,15 @@ impl Rp {
                     "Generating public key in {:?} from private key in {:?}",
                     public_keys_dir, private_keys_dir
                 );
+                Ok(())
             }
             Exchange {} => {
                 println!("Exchanging keys");
+                Ok(())
             }
         }
     }
 }
-
-
 
 fn main() {
     let rp = Rp::parse().run();
