1c0bfc154c755b4c5cdc6f9c3e8d20d472389d76
Karolin Varner <karo@cupdev.net>
2023-11-17 17:29:27 +0100

    chore: Move libsodium's increment into rosenpass-sodium crate

--- rosenpass-sodium/src/lib.rs
+++ rosenpass-sodium/src/lib.rs
@@ -34,3 +34,10 @@ pub fn compare(a: &[u8], b: &[u8]) -> i32 {
     assert!(a.len() == b.len());
     unsafe { libsodium::sodium_compare(a.as_ptr(), b.as_ptr(), a.len()) }
 }
+
+#[inline]
+pub fn increment(v: &mut [u8]) {
+    unsafe {
+        libsodium::sodium_increment(v.as_mut_ptr(), v.len());
+    }
+}
