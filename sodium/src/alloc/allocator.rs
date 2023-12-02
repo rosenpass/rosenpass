c5a2d23b554801dcb05eef714d55af3d713b56bc
Shylock Hg <tcath2s@gmail.com>
2023-12-02 20:20:38 +0800

    Separate secret allocation.

--- sodium/src/alloc/allocator.rs
+++ sodium/src/alloc/allocator.rs
@@ -12,61 +12,15 @@ struct AllocatorContents;
 #[derive(Clone, Default)]
 pub struct Alloc {
     _dummy_private_data: AllocatorContents,
-    fd : i32,
 }
 
 impl Alloc {
     pub fn new() -> Self {
-        let fd  = unsafe {
-            libc::syscall(libc::SYS_memfd_secret, 0) as i32
-        };
-        if fd == -1 {
-            panic!(
-                "Create secret file descriptor failed."
-            );
-        }
         Alloc {
             _dummy_private_data: AllocatorContents,
-            fd: fd,
         }
     }
 
-    fn do_secret_allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let ptr = unsafe {
-            let ret = libc::mmap(null_mut::<libc::c_void>(), layout.size(), libc::PROT_READ | libc::PROT_WRITE, libc::MAP_LOCKED, self.fd, 0);
-            if ret == libc::MAP_FAILED {
-                log::error!(
-                    "mmap failed."
-                );
-                return Err(AllocError);
-            } else {
-                ret
-            }
-        };
-        let off = ptr.align_offset(layout.align());
-        if off != 0 {
-            log::error!("Allocation {layout:?} was requested but mmap returned allocation \
-                with offset {off} from the requested alignment. mmap always allocates values \
-                at the end of a memory page for security reasons, custom alignments are not supported. \
-                You could try allocating an oversized value.");
-            Err(AllocError)
-        } else {
-            let ptr = core::ptr::slice_from_raw_parts_mut(ptr as *mut u8, layout.size());
-            match NonNull::new(ptr) {
-                None => {
-                    // Allocate failure have been processed in mmap.
-                    unreachable!()
-                }
-                Some(p) => Ok(p),
-            }
-        }
-    }
-
-    fn do_secret_deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
-        unsafe {
-            libc::munmap(ptr.as_ptr() as *mut libc::c_void, layout.size());
-        }
-    }
 }
 
 unsafe impl Allocator for Alloc {
@@ -112,7 +66,65 @@ impl fmt::Debug for Alloc {
     }
 }
 
-impl Drop for Alloc {
+#[derive(Clone, Default)]
+pub struct SecretAlloc {
+    fd : i32,
+}
+
+impl SecretAlloc {
+    pub fn new() -> Self {
+        let fd  = unsafe {
+            libc::syscall(libc::SYS_memfd_secret, 0) as i32
+        };
+        if fd == -1 {
+            panic!(
+                "Create secret file descriptor failed."
+            );
+        }
+        SecretAlloc {
+            fd: fd,
+        }
+    }
+
+    fn do_secret_allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        let ptr = unsafe {
+            let ret = libc::mmap(null_mut::<libc::c_void>(), layout.size(), libc::PROT_READ | libc::PROT_WRITE, libc::MAP_LOCKED, self.fd, 0);
+            if ret == libc::MAP_FAILED {
+                log::error!(
+                    "mmap failed."
+                );
+                return Err(AllocError);
+            } else {
+                ret
+            }
+        };
+        let off = ptr.align_offset(layout.align());
+        if off != 0 {
+            log::error!("Allocation {layout:?} was requested but mmap returned allocation \
+                with offset {off} from the requested alignment. mmap always allocates values \
+                at the end of a memory page for security reasons, custom alignments are not supported. \
+                You could try allocating an oversized value.");
+            Err(AllocError)
+        } else {
+            let ptr = core::ptr::slice_from_raw_parts_mut(ptr as *mut u8, layout.size());
+            match NonNull::new(ptr) {
+                None => {
+                    // Allocate failure have been processed in mmap.
+                    unreachable!()
+                }
+                Some(p) => Ok(p),
+            }
+        }
+    }
+
+    fn do_secret_deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+        unsafe {
+            libc::munmap(ptr.as_ptr() as *mut libc::c_void, layout.size());
+        }
+    }
+}
+
+impl Drop for SecretAlloc {
     fn drop(&mut self) {
         unsafe {
             libc::close(self.fd);
