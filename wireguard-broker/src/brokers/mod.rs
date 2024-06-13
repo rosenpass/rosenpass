511a59fe1bd6ec0876901d18ba9b55b02dd7cd69
Alice Bowman <alicebowman1993@gmail.com>
2024-06-13 13:18:23 +0200

    Alter target OS for mod.rs

--- wireguard-broker/src/brokers/mod.rs
+++ wireguard-broker/src/brokers/mod.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "enable_broker_api")]
 pub mod mio_client;
-#[cfg(feature = "enable_broker_api")]
+#[cfg(all(feature = "enable_broker_api", target_os = "linux"))]
 pub mod netlink;
 
 pub mod native_unix;
