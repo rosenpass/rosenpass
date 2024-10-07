00b1020f0a94e989cea5d04b2771338f4a5e67f5
wucke13 <wucke13@gmail.com>
2024-10-07 19:56:22 +0200

    refactor(flake.nix): externalize pkgs, add overlay
    This splits the complexity of the `flake.nix` into multiple files. At
    cross-compiled and static builds at the benefit of simpler nix
    expressions and generally better cross compilation compatibility.
    the same time, naersk is removed; causing much slower builds for cross-
    compiled packages.
    
    This partially addresses the points mentioned in #412.

---
+++ pkgs/rosenpass-oci-image.nix
@@ -0,0 +1,11 @@
+{ dockerTools, buildEnv, rosenpass }:
+
+dockerTools.buildImage {
+  name = rosenpass.name + "-oci";
+  copyToRoot = buildEnv {
+    name = "image-root";
+    paths = [ rosenpass ];
+    pathsToLink = [ "/bin" ];
+  };
+  config.Cmd = [ "/bin/rosenpass" ];
+}
