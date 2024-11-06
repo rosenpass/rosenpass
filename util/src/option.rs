6746a9b4407cb57afd6680cbaa2d8820e9a6f9ae
Paul Spooren <mail@aparcar.org>
2024-11-06 17:23:54 +0100

    docs(option): fix docstring warnings
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- util/src/option.rs
+++ util/src/option.rs
@@ -1,4 +1,6 @@
+/// A helper trait for turning any type value into `Some(value)`.
 pub trait SomeExt: Sized {
+    /// Wraps the calling value in `Some()`.
     fn some(self) -> Option<Self> {
         Some(self)
     }
