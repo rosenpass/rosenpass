0836a2eb2899ade873ffafa98c03e73b852472fa
Paul Spooren <mail@aparcar.org>
2024-11-06 17:31:37 +0100

    docs(zerocopy): fix docstring warnings
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- util/src/zerocopy/zerocopy_ref_ext.rs
+++ util/src/zerocopy/zerocopy_ref_ext.rs
@@ -1,10 +1,14 @@
 use zerocopy::{ByteSlice, ByteSliceMut, Ref};
 
+/// A trait for converting a `Ref<B, T>` into a `Ref<&[u8], T>`.
 pub trait ZerocopyEmancipateExt<B, T> {
+    /// Converts this reference into a reference backed by a byte slice.
     fn emancipate(&self) -> Ref<&[u8], T>;
 }
 
+/// A trait for converting a `Ref<B, T>` into a mutable `Ref<&mut [u8], T>`.
 pub trait ZerocopyEmancipateMutExt<B, T> {
+    /// Converts this reference into a mutable reference backed by a byte slice.
     fn emancipate_mut(&mut self) -> Ref<&mut [u8], T>;
 }
 
