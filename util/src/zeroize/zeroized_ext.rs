1209d687189d1d99d86c9c1c1d6b597935bd79d0
Paul Spooren <mail@aparcar.org>
2024-11-06 17:22:03 +0100

    docs(zeroize): fix docstring warnings
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- util/src/zeroize/zeroized_ext.rs
+++ util/src/zeroize/zeroized_ext.rs
@@ -1,6 +1,8 @@
 use zeroize::Zeroize;
 
+/// Extension trait providing a method for zeroizing a value and returning it
 pub trait ZeroizedExt: Zeroize + Sized {
+    /// Zeroizes the value in place and returns self
     fn zeroized(mut self) -> Self {
         self.zeroize();
         self
