69a8bc4bef55e956f97c5b75fd11a47db10e7f59
Paul Spooren <mail@aparcar.org>
2024-11-07 10:48:38 +0100

    chore(examples): add examples to docs
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- util/src/option.rs
+++ util/src/option.rs
@@ -1,4 +1,15 @@
 /// A helper trait for turning any type value into `Some(value)`.
+///
+/// # Examples
+///
+/// ```
+/// use rosenpass_util::option::SomeExt;
+///
+/// let x = 42;
+/// let y = x.some();
+///
+/// assert_eq!(y, Some(42));
+/// ```
 pub trait SomeExt: Sized {
     /// Wraps the calling value in `Some()`.
     fn some(self) -> Option<Self> {
