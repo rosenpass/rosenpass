231fa8f1497827800ee8bc17319c1d46d08e07d7
David Niehues <7667041+DavidNiehues@users.noreply.github.com>
2025-02-11 20:10:38 +0100

    tests(ciphers): add rudimentary tests for the shake256 implementation

--- ciphers/src/subtle/hash_functions/keyed_shake256.rs
+++ ciphers/src/subtle/hash_functions/keyed_shake256.rs
@@ -59,14 +59,47 @@ impl<const KEY_LEN: usize, const HASH_LEN: usize> KeyedHash<KEY_LEN, HASH_LEN> f
     }
 }
 
+
 impl<const KEY_LEN: usize, const HASH_LEN: usize> SHAKE256Core<KEY_LEN, HASH_LEN> {
     pub fn new() -> Self {
         Self
     }
 }
 
+/// TODO use nferred hash somehow here
+/// ```rust
+/// # use rosenpass_ciphers::subtle::keyed_shake256::{SHAKE256};
+/// use rosenpass_cipher_traits::KeyedHashInstance;
+/// const KEY_LEN: usize = 32;
+/// const HASH_LEN: usize = 32;
+/// let key: [u8; 32] = [0; KEY_LEN];
+/// let data: [u8; 32] = [255; 32]; // arbitrary data, could also be longer
+/// // buffer for the hash output
+/// let mut hash_data: [u8; 32] = [0u8; HASH_LEN];
+/// // TODO: Note that we are using inferred hash here
+/// assert!(SHAKE256::new().keyed_hash(&key, &data, &mut hash_data).is_ok(), "Hashing has to return OK result");
+/// # let expected_hash: &[u8] = &[44, 102, 248, 251, 141, 145, 55, 194, 165, 228, 156, 42, 220,
+/// 108, 50, 224, 48, 19, 197, 253, 105, 136, 95, 34, 95, 203, 149, 192, 124, 223, 243, 87];
+/// # assert_eq!(hash_data, expected_hash);
+/// ```
 pub type SHAKE256<const KEY_LEN: usize, const HASH_LEN: usize> =
 InferKeyedHash<SHAKE256Core<KEY_LEN, HASH_LEN>, KEY_LEN, HASH_LEN>;
 
-pub type SHAKE256_32<const KEY_LEN: usize, const OUT_LEN: usize> = SHAKE256<32, 32>;
+/// TODO: Documentation and more interesting test
+/// ```rust
+/// # use rosenpass_ciphers::subtle::keyed_shake256::{SHAKE256_32};
+/// use rosenpass_cipher_traits::KeyedHashInstance;
+/// const KEY_LEN: usize = 32;
+/// const HASH_LEN: usize = 32;
+/// let key: [u8; 32] = [0; KEY_LEN];
+/// let data: [u8; 32] = [255; 32]; // arbitrary data, could also be longer
+/// // buffer for the hash output
+/// let mut hash_data: [u8; 32] = [0u8; HASH_LEN];
+///
+/// assert!(SHAKE256_32::new().keyed_hash(&key, &data, &mut hash_data).is_ok(), "Hashing has to return OK result");
+/// # let expected_hash: &[u8] = &[44, 102, 248, 251, 141, 145, 55, 194, 165, 228, 156, 42, 220,
+/// 108, 50, 224, 48, 19, 197, 253, 105, 136, 95, 34, 95, 203, 149, 192, 124, 223, 243, 87];
+/// # assert_eq!(hash_data, expected_hash);
+/// ```
+pub type SHAKE256_32 = SHAKE256<32, 32>;
 
