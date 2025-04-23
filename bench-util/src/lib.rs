4cb76665b1b8da384870d2f7a40b783ebfe344d4
Jan Winkelmann (keks) <jan@cryspen.com>
2025-04-23 11:19:19 +0200

    use lazy_static instead of LazyLock (too new)

--- bench-util/src/lib.rs
+++ bench-util/src/lib.rs
@@ -1,9 +1,11 @@
-use std::{sync::LazyLock, time::Instant};
+use std::time::Instant;
 
 use libcrux_test_utils::tracing;
 
-/// The trace value used in all Rosepass crates.
-pub static TRACE: LazyLock<RpTrace> = LazyLock::new(|| RpTrace::default());
+lazy_static::lazy_static! {
+    /// The trace value used in all Rosepass crates.
+    pub static ref TRACE: RpTrace = RpTrace::default();
+}
 
 /// The trace type used to trace Rosenpass for performance measurement.
 pub type RpTrace = tracing::MutexTrace<&'static str, Instant>;
@@ -12,4 +14,3 @@ pub type RpTrace = tracing::MutexTrace<&'static str, Instant>;
 // [`libcrux_test_utils`].
 pub use libcrux_test_utils::tracing::trace_span;
 pub use tracing::Trace;
-
