70f046562332273af4b178cdce6ecee20b38d984
Jan Winkelmann (keks) <jan@cryspen.com>
2025-05-20 10:51:49 +0200

    fix flake.nix: no more fenix

--- flake.nix
+++ flake.nix
@@ -89,7 +89,6 @@
         [
           "x86_64-linux"
           "aarch64-linux"
-          "i686-linux"
         ]
         (
           system:
@@ -172,15 +171,6 @@
               env = {
                 inherit (pkgs.cargo-llvm-cov) LLVM_COV LLVM_PROFDATA;
               };
-            };
-            devShells.benchmark = pkgs.mkShell {
-              inputsFrom = [ pkgs.rosenpass ];
-              nativeBuildInputs = let
-                      rustToolchain = (inputs.fenix.packages.${system}.toolchainOf {
-                          channel = "1.77.0";
-                          sha256 = "sha256-+syqAd2kX8KVa8/U2gz3blIQTTsYYt3U63xBWaGOSc8=";
-                      });
-                  in [ rustToolchain.toolchain ];
             };
 
             checks =
