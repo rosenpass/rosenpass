0dbad2a3e282b6ab872b70f5523ac9ea44ac7706
Ilka Schulz <ilka@rosenpass.eu>
2026-08-16 11:02:19 +0200

    update doc comment of `rosenpass/src/lib.rs`

--- rosenpass/src/lib.rs
+++ rosenpass/src/lib.rs
@@ -1,26 +1,30 @@
 //! This is the central rosenpass crate implementing the rosenpass protocol.
 //!
-//! - [crate::app_server] contains the business logic of rosenpass, handling networking
-//! - [crate::cli] contains the cli parsing logic and contains quite a bit of startup logic; the
-//!   main function quickly hands over to [crate::cli::CliArgs::run] which contains quite a bit
+//! It contains the following modules:
+//! - [`crate::app_server`] contains the business logic of rosenpass, handling networking
+//! - [`crate::cli`] contains the cli parsing logic and contains quite a bit of startup logic; the
+//!   main function quickly hands over to [`crate::cli::CliArgs::run`] which contains quite a bit
 //!   of our startup logic
-//! - [crate::config] has the code to parse and generate configuration files
-//! - [crate::hash_domains] lists the different hash function domains used in the Rosenpass
+//! - [`crate::config`] has the code to parse and generate configuration files
+//! - [`crate::hash_domains`] lists the different hash function domains used in the Rosenpass
 //!   protocol
-//! - [crate::msgs] provides declarations of the Rosenpass protocol network messages and facilities
-//!   to parse those messages through the [::zerocopy] crate
-//! - [crate::protocol] this is where the bulk of our code lives; this module contains the actual
+//! - [`crate::msgs`] provides declarations of the Rosenpass protocol network messages and facilities
+//!   to parse those messages through the [`zerocopy`](::zerocopy) crate
+//! - [`crate::protocol`] this is where the bulk of our code lives; this module contains the actual
 //!   cryptographic protocol logic
-//! - crate::api implements the Rosenpass unix socket API, if feature "experiment_api" is active
+//! - [`crate::api`] implements the Rosenpass unix socket API, if feature `experiment_api` is enabled
+//! - [`crate::internal`] contains various internal modules which are only exposed if feature
+//!   `expose_internal_module` is enabled (see module description for explanation)
 
-#[cfg(feature = "experiment_api")]
-pub mod api;
 pub mod app_server;
 pub mod cli;
 pub mod config;
 pub mod hash_domains;
 pub mod msgs;
 pub mod protocol;
+
+#[cfg(feature = "experiment_api")]
+pub mod api;
 
 #[cfg(not(feature="expose_internal_modules"))]
 pub(crate) mod internal;
