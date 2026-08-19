329cd0da491694e147f36588816373019e8d4846
Ilka Schulz <ilka@rosenpass.eu>
2026-08-19 13:06:47 +0200

    get rid of feature flag for `rosenpass` binary

--- pkgs/rosenpass.nix
+++ pkgs/rosenpass.nix
@@ -67,7 +67,6 @@ rustPlatform.buildRustPackage {
   inherit src;
 
   buildFeatures = [
-    "rosenpass_binary"
     "rp"
   ];
   cargoBuildOptions = [
--- rosenpass/src/lib.rs
+++ rosenpass/src/lib.rs
@@ -44,25 +44,5 @@ pub(crate) mod internal;
 #[cfg(feature = "expose_internal_modules")]
 pub mod internal;
 
-/// Error types used in diverse places across Rosenpass
-#[derive(thiserror::Error, Debug)]
-pub enum RosenpassError {
-    /// Usually indicates that parsing a struct through the
-    /// [::zerocopy] crate failed
-    #[error("buffer size mismatch")]
-    BufferSizeMismatch,
-    /// Mostly raised by the `TryFrom<u8>` implementation for [crate::msgs::MsgType]
-    /// to indicate that a message type is not defined
-    #[error("invalid message type")]
-    InvalidMessageType(
-        /// The message type that could not be parsed
-        u8,
-    ),
-    /// Raised by the `TryFrom<RawMsgType>` (crate::api::RawMsgType) implementation for crate::api::RequestMsgType
-    /// and crate::api::RequestMsgType to indicate that a message type is not defined
-    #[error("invalid API message type")]
-    InvalidApiMessageType(
-        /// The message type that could not be parsed
-        u128,
-    ),
-}
+mod error;
+pub use error::RosenpassError;
