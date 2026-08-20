e26936695d2d9b1c4c20bde2e3d45c22a2326599
Ilka Schulz <ilka@rosenpass.eu>
2026-08-20 14:17:36 +0200

    fix required features for tests/rp_smoketest and use proper `test_bin` instead of `Command::new(env!("CARGO_BIN_EXE_rp"))`

--- rosenpass/tests/rp_smoketest.rs
+++ rosenpass/tests/rp_smoketest.rs
@@ -1,4 +1,5 @@
-use std::process::Command;
+/// name of the binary that we test
+const BINARY_NAME: &str = "rp";
 
 #[cfg(any(target_os = "linux", target_os = "freebsd"))]
 #[test]
@@ -20,27 +21,27 @@ fn smoketest() -> anyhow::Result<()> {
     "#;
 
     // Generate keys
-    let status = Command::new(env!("CARGO_BIN_EXE_rp"))
+    let status = test_bin::get_test_bin(BINARY_NAME)
         .args(["genkey", secret.to_str().unwrap()])
         .spawn()?
         .wait()?;
     assert!(status.success());
 
     // Derive Public keys
-    let status = Command::new(env!("CARGO_BIN_EXE_rp"))
+    let status = test_bin::get_test_bin(BINARY_NAME)
         .args(["pubkey", secret.to_str().unwrap(), public.to_str().unwrap()])
         .spawn()?
         .wait()?;
     assert!(status.success());
 
     // Can not exchange keys using exchange with invalid keys
-    let out = Command::new(env!("CARGO_BIN_EXE_rp"))
+    let out = test_bin::get_test_bin(BINARY_NAME)
         .args(["exchange", invalid.to_str().unwrap()])
         .output()?;
     assert!(!out.status.success());
 
     std::fs::write(toml, invalid_config)?;
-    let out = Command::new(env!("CARGO_BIN_EXE_rp"))
+    let out = test_bin::get_test_bin(BINARY_NAME)
         .args([
             "exchange-config",
             tmpdir.path().join("invalid_config").to_str().unwrap(),
